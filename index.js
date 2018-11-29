590f16fb0b6eed7f741a079e30568d92f8eeb4fb
mulicheryl <mintpepper13@yahoo.com>
2018-11-29 19:23:59 +0300

    add index.html styles.css index.js

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-function triangleTracker() {
+var first=parseInt(prompt("Enter a number!"));
+var second=parseInt(prompt("Now a second number!"));
+var third=parseInt(prompt("At last a third one"));
 
-
-// var firstValue = parseInt(document.getElementById('a').value);
-// var secondValue = parseInt(document.getElementById('b').value);
-// var thirdValue = parseInt(document.getElementById('c').value);
-// var outputs = document.getElementById("outputs");
-
-var firstValue = document.getElementById('a').value;
-var secondValue = document.getElementById('b').value;
-var thirdValue = document.getElementById('c').value;
-
-if(firstValue==secondValue && secondValue==thirdValue && thirdValue==firstValue){
-  alert("equilateral triangle");
-}
-else if(firstValue==secondValue || secondValue==thirdValue || thirdValue==firstValue){
-  alert("isosceles");
-}
-else if(firstValue!==secondValue!==thirdValue){
-  alert("scalene");
-}
-else if(firstValue + secondValue <= thirdValue || firstValue + thirdValue <= secondValue || secondValue + thirdValue <= firstValue) {
-  alert("not a triangle");
-}
-else {
-  alert("enter valid values");
-}
+if(first === second && second === third && first === third );
+   return(equilateral);
+    alert(An equilateral);
